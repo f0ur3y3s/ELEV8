3ff1779eb55130f7843b447030fd3f1364cada65
incub4t0r <dchung0812@gmail.com>
2023-09-02 22:25:09 -0400

    switching machines

--- backend/server.py
+++ backend/server.py
@@ -29,64 +29,94 @@ app.add_middleware(
 db = Database("workouts.sqlite")
 
 class Workout(BaseModel):
+    user_id: int
+    name: str
+
+class Exercise(BaseModel):
     name: str
     bodypart: str
+
+class WorkoutExercise(BaseModel):
+    workout_id: int
+    exercise_id: int
+
+class SetReps(BaseModel):
+    workout_exercise_id: int
+    sets: int
     reps: int
     weight: int
 
-class Update(BaseModel):
-    id: int
+class User(BaseModel):
+    username: str
+    password: str
     name: str
-    bodypart: str
-    reps: int
-    weight: int
+    email: str
+    bio: str
+
+class Login(BaseModel):
+    username: str
+    password: str
 
 class GroupBy(BaseModel):
     bodypart: str
+
 #################################
 # FastAPI Endpoints
 #################################
 
-# create
-@app.post("/create")
-async def create(workout: Workout, request: Request):
+# create workout
+@app.post("/create/workout")
+async def create_workout(workout: Workout, request: Request):
     print(workout.name)
-    db.insert(workout.name, workout.bodypart, workout.reps, workout.weight)
-    return {"message": "Create"}
+    db.create_workout(workout.user_id, workout.name)
+    return {"message": "Create Workout"}
 
-# read
-@app.get("/read")
-async def read(request: Request):
-    workouts = db.view()
-    # parse workouts into proper json
-    data = {}
-    data["workouts"] = workouts
-    return data
+@app.post("/create/exercise")
+async def create_exercise(exercise: Exercise, request: Request):
+    print(exercise.name)
+    db.create_exercise(exercise.name, exercise.bodypart)
+    return {"message": "Create Exercise"}
 
-@app.get("/groupby")
-async def groupby(bodypart: GroupBy, request: Request):
-    workouts = db.filter(bodypart)
-    # parse workouts into proper json
-    data = {}
-    data[bodypart] = workouts
-    return data
-
-
-# update
-@app.put("/update")
-# async def update(workout: Workout, request: Request):
+# # create
+# @app.post("/create")
+# async def create(workout: Workout, request: Request):
 #     print(workout.name)
-#     return {"message": "Update"}
-async def update(toUpdate: Update, request: Request):
-    # print(toUpdate.name)
-    db.update(toUpdate.id, toUpdate.name, toUpdate.bodypart, toUpdate.reps, toUpdate.weight)
-    return {"message": "Update"}
+#     db.insert(workout.name, workout.bodypart, workout.reps, workout.weight)
+#     return {"message": "Create"}
 
-# delete
-@app.delete("/delete")
-async def delete(workout: Workout, request: Request):
-    print(workout.name)
-    return {"message": "Delete"}
+# # read
+# @app.get("/read")
+# async def read(request: Request):
+#     workouts = db.view()
+#     # parse workouts into proper json
+#     data = {}
+#     data["workouts"] = workouts
+#     return data
+
+# @app.get("/groupby")
+# async def groupby(bodypart: GroupBy, request: Request):
+#     workouts = db.filter(bodypart)
+#     # parse workouts into proper json
+#     data = {}
+#     data[bodypart] = workouts
+#     return data
+
+
+# # update
+# @app.put("/update")
+# # async def update(workout: Workout, request: Request):
+# #     print(workout.name)
+# #     return {"message": "Update"}
+# async def update(toUpdate: Update, request: Request):
+#     # print(toUpdate.name)
+#     db.update(toUpdate.id, toUpdate.name, toUpdate.bodypart, toUpdate.reps, toUpdate.weight)
+#     return {"message": "Update"}
+
+# # delete
+# @app.delete("/delete")
+# async def delete(workout: Workout, request: Request):
+#     print(workout.name)
+#     return {"message": "Delete"}
 
 
 if __name__ == "__main__":
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    
+</script>
+
+<!-- bootstrap navbar -->
+
+<div class="container">
+    <!-- navbar -->
+    Welcome to elev8
+    <!-- form for workouts -->
+    <form action="#">
+        <input type="text">
+    </form>
+</div>
